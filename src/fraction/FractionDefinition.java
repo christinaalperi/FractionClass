fce2c2c5189958f5baf7c5faf56a453e5efdbe49
chris <chris@DESKTOP-KK4UKFU>
2019-01-03 18:00:54 -0500

    second commit

--- src/fraction/FractionDefinition.java
+++ src/fraction/FractionDefinition.java
@@ -1,4 +1,4 @@
-package main;
+package fraction;
 
 public class FractionDefinition {
 	int numerator;
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,8 @@
 package main;
 
-public class FractionClass {
+import fraction.FractionDefinition;
+
+public class Main {
 	
 	public static void main(String[] args) {
 		FractionDefinition a = new FractionDefintion(1,2);
